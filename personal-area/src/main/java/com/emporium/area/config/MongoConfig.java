8ea10f5b5939af8f4b0ff8f4a3c4c6de244e0565
Ilya Okonechnikov <ok.ilya95kazan@mail.ru>
2020-04-01 19:51:14 +0300

    prepared Dockerfile, got rid of 'flapdoodle' lib

--- personal-area/src/main/java/com/emporium/area/config/MongoConfig.java
+++ personal-area/src/main/java/com/emporium/area/config/MongoConfig.java
@@ -1,7 +1,6 @@
 package com.emporium.area.config;
 
 import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
 import com.mongodb.ServerAddress;
 import com.mongodb.client.MongoClient;
 import com.mongodb.client.MongoClients;
@@ -23,23 +22,16 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
 
   private final String host;
   private final Integer port;
-  private final String username;
-  private final String password;
   private final String database;
 
   public MongoConfig(@Value("${spring.data.mongodb.host}") String host,
                      @Value("${spring.data.mongodb.port}") Integer port,
-                     @Value("${spring.data.mongodb.username}") String username,
-                     @Value("${spring.data.mongodb.password}") String password,
                      @Value("${spring.data.mongodb.database}") String database) {
     this.host = host;
     this.port = port;
-    this.username = username;
-    this.password = password;
     this.database = database;
     log.info("MongoConfig initialized with the following credentials: \n" +
-            "host: {} \nport: {} \nusername: {} \npassword: {} \ndatabase: {}. ",
-        host, port, username, password, database);
+            "host: {} \nport: {} \ndatabase: {}. ", host, port, database);
   }
 
   @Override
@@ -51,7 +43,6 @@ public class MongoConfig extends AbstractMongoClientConfiguration {
   @Override
   public MongoClient mongoClient() {
     MongoClientSettings clientSettings = MongoClientSettings.builder()
-        .credential(MongoCredential.createCredential(username, database, password.toCharArray()))
         .applyToClusterSettings(settings -> settings.hosts(Collections.singletonList(new ServerAddress(host, port))))
         .build();
     return MongoClients.create(clientSettings);
--- personal-area/src/test/java/com/emporium/area/TestContextConfig.java
+++ personal-area/src/test/java/com/emporium/area/TestContextConfig.java
@@ -1,18 +1,14 @@
 package com.emporium.area;
 
-import com.emporium.area.repository.UserRepository;
-
 import org.springframework.boot.SpringBootConfiguration;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.context.annotation.ComponentScan;
-import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootConfiguration
 @EnableAutoConfiguration
-@EnableMongoRepositories(basePackageClasses = UserRepository.class)
 @ComponentScan(basePackages = {
-    "com.emporium.area.service",
-    "com.emporium.area.repository",
+    "com.emporium.area.config",
+    "com.emporium.area.service"
 })
 public class TestContextConfig {
 
