4294349169c95884210ed5b52411777be28d10a2
airat-git <airat_f17@mail.ru>
2020-12-25 15:37:38 +0300

    flyway added

--- ad/src/main/java/com/emporium/ad/AdApplication.java
+++ ad/src/main/java/com/emporium/ad/AdApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class AdApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(AdApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(AdApplication.class, args);
+  }
 }
