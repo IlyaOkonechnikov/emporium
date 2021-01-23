f9f6e50bb78208d1fefaf3699980eb22aae5e7a9
airat-git <airat_f17@mail.ru>
2021-01-23 23:45:46 +0300

    changes for controller annotations

--- ad/src/main/java/com/emporium/ad/controller/CategoryController.java
+++ ad/src/main/java/com/emporium/ad/controller/CategoryController.java
@@ -8,7 +8,6 @@ import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
-import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import lombok.RequiredArgsConstructor;
 
 import org.springframework.http.HttpStatus;
@@ -26,49 +25,42 @@ public class CategoryController {
   private final CategoryService categoryService;
 
   @GetMapping
-  @ApiResponse(responseCode = "200", description = "All categories found", content = {
-      @Content(mediaType = "application/json", schema = @Schema(implementation = Category.class))})
+  @DefaultResponses
   public List<Category> findAll() {
     return categoryService.findAll();
   }
 
   @GetMapping("/main-categories")
-  @ApiResponse(responseCode = "200", description = "Main categories found", content = {
-      @Content(mediaType = "application/json", schema = @Schema(implementation = Category.class))})
+  @DefaultResponses
   public List<Category> findMainCategories() {
     return categoryService.findMainCategories();
   }
 
   @GetMapping("/{id}")
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Category found", content = {
-          @Content(mediaType = "application/json", schema = @Schema(implementation = Category.class))}),
-      @ApiResponse(responseCode = "500", description = "Category not found", content = @Content)})
+  @DefaultResponses
   public Category findById(@Parameter(description = "id of category to be searched") @PathVariable int id) {
     return categoryService.findById(id);
   }
 
   @PostMapping
   @ResponseStatus(HttpStatus.CREATED)
-  @ApiResponse(responseCode = "201", description = "Category created", content = @Content)
+  @DefaultResponses
+  @ApiResponse(responseCode = "201", description = "Successful creation", content = {
+      @Content(mediaType = "application/json", schema = @Schema(implementation = Category.class))})
   public String create(@RequestBody @Valid CategoryDTO dto) {
     return categoryService.create(dto);
   }
 
   @PutMapping
   @ResponseStatus(HttpStatus.OK)
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Category updated", content = @Content),
-      @ApiResponse(responseCode = "500", description = "Category not found", content = @Content)})
+  @DefaultResponses
   public void update(@RequestBody @Valid CategoryDTO dto) {
     categoryService.update(dto);
   }
 
   @DeleteMapping("/{id}}")
   @ResponseStatus(HttpStatus.OK)
-  @ApiResponses(value = {
-      @ApiResponse(responseCode = "200", description = "Category deleted"),
-      @ApiResponse(responseCode = "500", description = "Category not found")})
+  @DefaultResponses
   public void delete(@Parameter(description = "id of category to be deleted") @PathVariable int id) {
     categoryService.delete(id);
   }
